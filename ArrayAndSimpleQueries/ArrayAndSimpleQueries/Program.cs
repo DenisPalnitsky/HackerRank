8b1eb09d6cbb2563ec080e837a2e9d49fc0dfb25
Denis <palnitsky@gmail.com>
2016-07-19 17:42:45 +0200

    Update Program.cs

--- ArrayAndSimpleQueries/ArrayAndSimpleQueries/Program.cs
+++ ArrayAndSimpleQueries/ArrayAndSimpleQueries/Program.cs
@@ -8,6 +8,115 @@ namespace ArrayAndSimpleQueries
 {
     class Program
     {
+              [DebuggerDisplay("Val={val}, Prior={prior}")]
+        class Node
+        {
+            public int val;
+            public int prior;
+
+            public Node l;
+            public Node r;
+        }
+
+        static Random randomizer = new Random();
+
+        static void split(Node t, ref Node l, ref Node r, int key)
+        {
+            if (t == null)
+            {
+                l = null;
+                r = null;
+            }
+            else if (t.val <= key)
+            {
+                split(t.r, ref t.r, ref r, key);
+                l = t; //elem=key comes in l
+            }
+            else
+            {
+                split(t.l, ref l, ref t.l, key);
+                r = t;
+            }
+
+        }
+
+        void merge(ref Node t, Node l, Node r)
+        {
+            if (l == null || r == null)
+                t = l ?? r;
+            else if (l.prior > r.prior)
+            {
+                merge(ref l.r, l.r, r);
+                t = l;
+            }
+            else
+            {
+                merge(ref r.l, l, r.l);
+                t = r;
+            }
+        }
+
+        static void insert(ref Node t, Node it)
+        {
+            if (t == null)
+                t = it;
+            else if (it.prior > t.prior)
+            {
+                split(t, ref it.l, ref it.r, it.val);
+                t = it;
+            }
+            else
+            {
+                var node = t.val <= it.val ? t.r : t.l;
+                insert(ref node, it);
+            }
+        }
+
+        static Node init(int val)
+        {
+            Node ret = new Node();
+            ret.val = val;
+            ret.prior = randomizer.Next();
+            return ret;
+        }
+
+        static void AddL(ref Node l)
+        {
+            l = new Node() { val = 5 };
+        }
+
+        static void Main(string[] args)
+        {            
+            var root = init(10);
+            var node5 = init(5);
+            var node20 = init(20);
+
+            insert(ref root, node5);
+            print(root);
+
+            insert(ref root, node20);
+            print(root);
+
+            Console.ReadKey();
+        }
+
+        static void print (Node node)
+        {
+            traverse(node);
+            Console.WriteLine();
+        }
+
+        static void traverse(Node root)
+        {
+            if (root != null)
+            {
+                Console.Write("[{0}:{1}],", root.val, root.prior);
+                traverse(root.l);
+                traverse(root.r);
+            }
+        }
+        
+        
 
         class Query
         {
