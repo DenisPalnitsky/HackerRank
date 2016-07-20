b0592f9cc4c93cab0590310cfb6ffbf8e5dbbcbd
Denis <denis.palnitsky@gmail.com>
2016-07-20 23:38:20 +0200

    Treap implementation

--- ArrayAndSimpleQueries/ArrayAndSimpleQueries/Program.cs
+++ ArrayAndSimpleQueries/ArrayAndSimpleQueries/Program.cs
@@ -7,117 +7,7 @@ using System.Threading.Tasks;
 namespace ArrayAndSimpleQueries
 {
     class Program
-    {
-              [DebuggerDisplay("Val={val}, Prior={prior}")]
-        class Node
-        {
-            public int val;
-            public int prior;
-
-            public Node l;
-            public Node r;
-        }
-
-        static Random randomizer = new Random();
-
-        static void split(Node t, ref Node l, ref Node r, int key)
-        {
-            if (t == null)
-            {
-                l = null;
-                r = null;
-            }
-            else if (t.val <= key)
-            {
-                split(t.r, ref t.r, ref r, key);
-                l = t; //elem=key comes in l
-            }
-            else
-            {
-                split(t.l, ref l, ref t.l, key);
-                r = t;
-            }
-
-        }
-
-        void merge(ref Node t, Node l, Node r)
-        {
-            if (l == null || r == null)
-                t = l ?? r;
-            else if (l.prior > r.prior)
-            {
-                merge(ref l.r, l.r, r);
-                t = l;
-            }
-            else
-            {
-                merge(ref r.l, l, r.l);
-                t = r;
-            }
-        }
-
-        static void insert(ref Node t, Node it)
-        {
-            if (t == null)
-                t = it;
-            else if (it.prior > t.prior)
-            {
-                split(t, ref it.l, ref it.r, it.val);
-                t = it;
-            }
-            else
-            {
-                var node = t.val <= it.val ? t.r : t.l;
-                insert(ref node, it);
-            }
-        }
-
-        static Node init(int val)
-        {
-            Node ret = new Node();
-            ret.val = val;
-            ret.prior = randomizer.Next();
-            return ret;
-        }
-
-        static void AddL(ref Node l)
-        {
-            l = new Node() { val = 5 };
-        }
-
-        static void Main(string[] args)
-        {            
-            var root = init(10);
-            var node5 = init(5);
-            var node20 = init(20);
-
-            insert(ref root, node5);
-            print(root);
-
-            insert(ref root, node20);
-            print(root);
-
-            Console.ReadKey();
-        }
-
-        static void print (Node node)
-        {
-            traverse(node);
-            Console.WriteLine();
-        }
-
-        static void traverse(Node root)
-        {
-            if (root != null)
-            {
-                Console.Write("[{0}:{1}],", root.val, root.prior);
-                traverse(root.l);
-                traverse(root.r);
-            }
-        }
-        
-        
-
+    {        
         class Query
         {
             public int QueryType;
@@ -131,13 +21,33 @@ namespace ArrayAndSimpleQueries
                 J = j;
             }
         }
-
+       
         static void Main(string[] args)
         {
+            var root = Treap.init(50,15);                                    
+            var node2 = Treap.init(30, 5);            
+            var node3 = Treap.init(70, 10);
+            var node4 = Treap.init(20, 2);
+            var node5 = Treap.init(40, 4);                    
+
+            Treap.insert(ref root, node2);
+            Treap.insert(ref root, node3);
+            Treap.insert(ref root, node4);
+            Treap.insert(ref root, node5);
+            Treap.print(root);                                    
+
+            Console.ReadKey();
+
+
+        }
+
+        private static void Solution()
+        {
+
             var line1 = readIntArrayFormInput();
 
             var array = readIntArrayFormInput();
-            
+
             for (int i = 0; i < line1[1]; i++)
             {
                 var queryInts = readIntArrayFormInput();
---
+++ ArrayAndSimpleQueries/ArrayAndSimpleQueries/Treap.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayAndSimpleQueries
+{
+    [DebuggerDisplay("Val={val}, Prior={prior}")]
+    class Node
+    {
+        public int val;
+        public int prior;
+
+        public Node l;
+        public Node r;
+    }
+
+    class Treap
+    {        
+        static Random randomizer = new Random();
+
+        public static void split(Node t, ref Node l, ref Node r, int key)
+        {
+            if (t == null)
+            {
+                l = null;
+                r = null;
+            }
+            else if (t.val <= key)
+            {
+                split(t.r, ref t.r, ref r, key);
+                l = t; //elem=key comes in l
+            }
+            else
+            {
+                split(t.l, ref l, ref t.l, key);
+                r = t;
+            }
+
+        }
+
+        public void merge(ref Node t, Node l, Node r)
+        {
+            if (l == null || r == null)
+                t = l ?? r;
+            else if (l.prior > r.prior)
+            {
+                merge(ref l.r, l.r, r);
+                t = l;
+            }
+            else
+            {
+                merge(ref r.l, l, r.l);
+                t = r;
+            }
+        }
+
+        public static void insert(ref Node t, Node it)
+        {
+            if (t == null)
+                t = it;
+            else if (it.prior > t.prior)
+            {
+                split(t, ref it.l, ref it.r, it.val);
+                t = it;
+            }
+            else
+            {                
+                if (t.val <= it.val)
+                    insert(ref t.r, it);
+                else 
+                    insert (ref t.l, it);
+            }
+        }
+
+        public static Node init(int val)
+        {
+            Node ret = new Node();
+            ret.val = val;
+            ret.prior = randomizer.Next(10);
+            return ret;
+        }
+
+        public static Node init(int val, int priority)
+        {
+            Node ret = new Node();
+            ret.val = val;
+            ret.prior = priority;
+            return ret;
+        }
+
+        public static void AddL(ref Node l)
+        {
+            l = new Node() { val = 5 };
+        }
+
+        public static void print(Node node)
+        {
+            traverse(node);
+            Console.WriteLine();
+        }
+
+        public static void traverse(Node root)
+        {
+            if (root != null)
+            {
+                Console.Write("[{0}:{1}],", root.val, root.prior);
+                traverse(root.l);
+                traverse(root.r);
+            }
+        }
+        
+    }
+}
