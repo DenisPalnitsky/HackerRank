128f69d7741c9377cb84bb455a9aab28ae61920a
Denis Palnitsky <palnitsky@gmail.com>
2017-09-02 00:24:28 +0200

    BreathFirstSearch algorithm implementation

--- ShortestPathInAField/ShortestPathInAField/Program.cs
+++ ShortestPathInAField/ShortestPathInAField/Program.cs
@@ -1,25 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ShortestPathInAField
 {
     class Program
     {
         static int[,] field = new int[4, 4] {
-                {0, 1, 0, 0},
-                {1, 1, 0, 0},
                 {0, 0, 0, 0},
+                {1, 0, 1, 0},
+                {0, 0, 1, 0},
                 {0, 0, 0, 0}};
 
         static void Main(string[] args)
         {
             
-
-
-            var startPoint = new[] { 0, 0 };
+            var startPoint = new[] { 2, 0 };
             var endPoint = new[] { 0, 0 };
 
             ShortesPathLength(field, startPoint, endPoint);
@@ -29,40 +24,16 @@ namespace ShortestPathInAField
         {
             var adjList = ConvertToAdjencyMatrix(field);
 
-            Console.WriteLine($"Shortest path: {BFS(adjList, startPoint, endPoint)}"); 
-          
-            Console.WriteLine(adjList.Count);
+
+            int c = BreathFirstSearch.BFS(adjList,
+                11,
+                8);
+
+            Console.WriteLine(" Result = " + c);
+            Console.ReadKey();
         }
 
-        private static int BFS(Dictionary<int, HashSet<int>> adjList, int[] startPoint, int[] endPoint)
-        {
-            Queue<int> visited = new Queue<int>();
-            int count = 0;
-
-            return bfsRecursive(adjList, 
-                GetCoordinateId(startPoint[0], startPoint[1]),
-                GetCoordinateId(endPoint[0], endPoint[1]) , visited, count);
-            
-        }
-
-        private static int bfsRecursive(Dictionary<int, HashSet<int>> adjList, int startPointId, int endPointId, Queue<int> visited, int count)
-        {
-            if (visited.Contains(startPointId))
-            {
-                return count;
-            }
-
-            count++;
-            visited.Enqueue(startPointId);
-
-
-            foreach (var v in adjList[startPointId])
-            {
-                bfsRecursive(adjList, v, endPointId, visited, count);
-            }
-
-            return count;
-        }
+     
 
         private static Dictionary<int, HashSet<int>> ConvertToAdjencyMatrix(int[,] field)
         {            
@@ -76,18 +47,10 @@ namespace ShortestPathInAField
                     {
                         var adj = new HashSet<int>();
 
-                        if (IsEdge(i - 1, j - 1, field))
-                            adj.Add(GetCoordinateId(i, j - 1));
-
-                        if (IsEdge(i - 1, j, field))
-                            adj.Add(GetCoordinateId(i - 1, j));
-
-                        if (IsEdge(i + 1, j + 1, field))
-                            adj.Add(GetCoordinateId(i + 1, j));
-
-                        if (IsEdge(i + 1, j + 1, field))
-                            adj.Add(GetCoordinateId(i, j + 1));
-
+                        AddAdj(field, i-1 , j, adj);
+                        AddAdj(field, i + 1, j, adj);
+                        AddAdj(field, i , j-1, adj);
+                        AddAdj(field, i , j+1, adj);
                         edges.Add(GetCoordinateId(i, j), adj);
                     }
                 }
@@ -95,7 +58,13 @@ namespace ShortestPathInAField
                         
             return edges;
         }
-        
+
+        private static void AddAdj(int[,] field, int i, int j, HashSet<int> adj)
+        {
+            if (IsEdge(i, j , field))
+                adj.Add(GetCoordinateId(i, j ));
+        }
+
         static bool IsEdge(int x, int y, int[,] field)
         {
             if(x>= 0 && x< field.GetLength(0) && y>=0 && y< field.GetLength(1) )
@@ -106,13 +75,9 @@ namespace ShortestPathInAField
             return false;
         }
 
-        static int GetCoordinateId( int x, int y)
-        {
-            if (y <= 0)
-                return x;
-
-            return field.GetLength(0)* y  + x ;
-            
+        public static int GetCoordinateId( int x, int y)
+        {        
+            return field.GetLength(1) * x  + y ;            
         }
     }
 }
